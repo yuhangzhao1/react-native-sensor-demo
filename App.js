db16756555275e3c88103fd2e02611a459b919ed
yuhangzhao1 <75333593+yuhangzhao1@users.noreply.github.com>
2021-11-08 17:03:25 -0600

    create sensor demo

--- App.js
+++ App.js
@@ -1,21 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Value = ({name, value}) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.valueName}>{name}:</Text>
+    <Text style={styles.valueValue}>{new String(value).substr(0, 8)}</Text>
+  </View>
+)
+
+export default class AccelerometerSensor extends React.Component {
+  state = {
+    accelerometerData: {},
+    buttonState: "Stop"
+  };
+
+  _subscription = null;
+
+  componentDidMount() {
+    this._toggle();
+    Accelerometer.setUpdateInterval(250);
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _toggle = () => {
+    if (this._subscription) {
+      this._unsubscribe();
+      this.setState({buttonState: "Start"});
+    } else {
+      this._subscribe();
+      this.setState({buttonState: "Stop"});
+    }
+  };
+
+  _subscribe = () => {
+    this._subscription = Accelerometer.addListener(accelerometerData => {
+      this.setState({ accelerometerData });
+    });
+  };
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+  render() {
+    let { x, y, z } = this.state.accelerometerData;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headline}>Accelerometer</Text>
+        <Value name="x" value={x} />
+        <Value name="y" value={y} />
+        <Value name="z" value={z} />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this._toggle} style={styles.button}>
+            <Text style={styles.buttonText}>{this.state.buttonState}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 100) / 100;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  headline: {
+    fontSize: 32,
+    textAlign: 'center',
+    margin: 10,
+  },
+  valueContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  valueValue: {
+    width: 100,
+    fontSize: 20
+  },
+  valueName: {
+    width: 50,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d0d5d6',
+    padding: 10,
+    marginLeft: 100,
+    marginRight: 100,
+    borderRadius:10,
+  },
+  buttonText: {
+    fontSize: 20,
   },
 });
